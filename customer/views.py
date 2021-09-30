5e7cb7d1d6a8e2c3c51e0fb3b08c31145ba4bb4e
sawad kk <ssawad09@gmail.com>
2021-09-30 14:11:21 +0530

    'ticket_plan

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from theater.models import *
 from .models import *
+from datetime import date
+
+global today 
+today = date.today()
 
 def index(request):
 	movies = Movie.objects.all()
@@ -8,23 +12,40 @@ def index(request):
 
 def movie_details(request, movie_pk):
 	movie = Movie.objects.filter(id=movie_pk)
-	show = Show.objects.filter(movie_id=movie_pk) 
-	return render(request,'customers/movie_details.html',{'movie':movie,'shows':show})
+	return render(request,'customers/movie_details.html',{'movie':movie})
 
-def ticket_plan(request, show_pk):
-	show = Show.objects.filter(id=show_pk) 
-	return render(request,'customers/ticket_plan.html',{'shows':show})
-
+def ticket_plan(request, movie_pk):
+	print('sawad')
+	movie = Movie.objects.filter(id=movie_pk)
+	show = Show.objects.filter(date=today,movie__in=movie)
+	show2 = Show.objects.filter(movie__in=movie)
+	print(show)
+	dates = []
+	for i in show2:
+		date = str(i.date)
+		dates.append(date)
+	print(dates)
+	
+	return render(request,'customers/ticket_plan.html',{'shows':show,'movie':movie,'dates':dates})
 def load_data(request):
 	if request.method == 'POST':
-		location = request.POST.get('location')
-		print(location)
-		date = request.POST.get('date')
-		print(date)
-		show_pk = request.POST.get('show_pk')
-		print(show_pk)
-		show = Show.objects.filter(id=show_pk,date=date)
-		for i in show:
-			print(i.time)
-		return render(request,'customers/ticket_plan.html',{'shows':show})
+		location = request.POST['location']
+		date2 = request.POST['date']
+		print(date2)
+		movie_pk = request.POST['pk']
+		#print(movie_pk)
+		movie = Movie.objects.filter(id=movie_pk)
+		#print(movie)
+		show = Show.objects.filter(date=date2,movie__in=movie)
+		show2 = Show.objects.filter(movie__in=movie)
+		dates = []
+		for i in show2:
+			date = str(i.date)
+			dates.append(date)
+		print(dates)
+		return render(request,'customers/load_data.html',{'shows':show,'movie':movie,'dates':dates})
 	return redirect('index')
+
+def seat_plan(request, show_pk):
+	show = Show.objects.filter(id=show_pk)
+	return render(request,'customers/seat_plan.html',{'shows':show})
