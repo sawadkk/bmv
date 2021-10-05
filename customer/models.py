52b4837b87f5ab2140b12dd2ab63fee88653b977
sawad kk <ssawad09@gmail.com>
2021-10-05 17:17:28 +0530

    'seat_booking'_

--- customer/models.py
+++ customer/models.py
@@ -4,22 +4,11 @@ from theater.models import *
 
 class Pending(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-	seat_number = models.IntegerField() 
+	show = models.ForeignKey(Show, on_delete=models.CASCADE)
+	seat_number = models.CharField(max_length=50) 
 
 class Booked(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-	seat_number = models.IntegerField()
+	show = models.ForeignKey(Show, on_delete=models.CASCADE)
+	seat_number = models.CharField(max_length=50)
 
-class Show(models.Model):
-	screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
-	theater = models.ForeignKey(User, on_delete=models.CASCADE)
-	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-	#start_date = models.DateField()
-	#end_date = models.DateField()
-	date = models.DateField()
-	time = models.TimeField()
-
-	def __str__(self):
-		return  self.screen.screen_name +" | "+ self.movie.movie_name +" | ", self.time
